588b8dbe9449ff476922789b7f6753d9fd2639a4
huyviet1995 <vietdanghuy.95@gmail.com>
2016-02-14 17:08:18 -0600

    no change

--- app/src/main/java/com/example/huyviet1995/myapplication/QuizActivity.java
+++ app/src/main/java/com/example/huyviet1995/myapplication/QuizActivity.java
@@ -87,14 +87,7 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-        mPreviousButton = (Button)findViewById(R.id.previous_button);
-        mPreviousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionTextView.length();
-                updateQuestion();
-            }
-        });
+
     }
 
 }
