244ac6a0f433105537efafc604a0778c17d6bc5a
huyviet1995 <vietdanghuy.95@gmail.com>
2016-02-14 16:49:55 -0600

    Initial stage of the project

--- app/src/main/java/com/example/huyviet1995/myapplication/QuizActivity.java
+++ app/src/main/java/com/example/huyviet1995/myapplication/QuizActivity.java
@@ -81,14 +81,14 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mPreviousButton = (Button)findViewById(R.id.previous_button);
+        /*mPreviousButton = (Button)findViewById(R.id.previous_button);
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
         public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex - 1) % mQuestionTextView.length();
                 updateQuestion();
             }
-        });
+        });*/
 
     }
 
